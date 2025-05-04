ac18efc885b64bb5a50143e605716b57ea3e5b87
Truong-Phuc Nguyen <99306941+ntphuc149@users.noreply.github.com>
2025-05-04 16:11:49 +0700

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,9 @@
 """Training script for the ViAG project."""
 
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import logging
 import argparse
 import json
